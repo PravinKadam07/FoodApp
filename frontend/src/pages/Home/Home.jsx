462182ca4388f2c429fb81ca27dc946d84bad9e4
Pravin Kadam <pravinkadam21998@gmail.com>
2024-06-01 15:31:52 +0530

    expore our menu section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../componants/Header/Header";
+import ExploreMenu from "../../componants/ExploreMenu/ExploreMenu";
 function Home() {
+  const [category, setCategory] = useState("All");
   return (
     <div>
-      {" "}
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 }
