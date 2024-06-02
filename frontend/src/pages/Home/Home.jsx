abda98cbb5116ad1c789442a85d26ca326bfb8e7
Pravin Kadam <pravinkadam21998@gmail.com>
2024-06-02 18:43:41 +0530

    food-items section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../componants/Header/Header";
 import ExploreMenu from "../../componants/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../componants/FoodDisplay/FoodDisplay";
 function Home() {
   const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 }
